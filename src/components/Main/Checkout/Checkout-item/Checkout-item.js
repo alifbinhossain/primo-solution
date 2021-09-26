3b7623a0b755069b5ce37e97b356d7e2890744ab
alifbinhossain <you@example.com>
2021-09-26 22:08:10 +0600

    css added

--- src/components/Main/Checkout/Checkout-item/Checkout-item.js
+++ src/components/Main/Checkout/Checkout-item/Checkout-item.js
@@ -3,14 +3,23 @@ import "./Checkout-item.css";
 
 const CheckoutItem = (props) => {
   const { advisor } = props;
-  const { firstName, lastName, charge, photo } = advisor;
+  const { firstName, lastName, charge, photo, jobTitle } = advisor;
   return (
     <li className="list-item d-flex justify-content-between align-items-center">
       <img src={photo} alt="" />
-      <small>
-        {firstName} {lastName}
-      </small>
-      <small className="text-warning">${charge}</small>
+      <div className="item-user d-flex align-items-center justify-content-between">
+        <small>
+          <span className="text-bold">
+            {firstName} {lastName}{" "}
+          </span>
+          <br />
+          <span className="job-title">{jobTitle.slice(0, 18)}..</span>
+        </small>
+        <small>
+          <i class="fas fa-check-circle check-circle"></i>
+        </small>
+      </div>
+      {/* <small className="text-warning">${charge}</small> */}
     </li>
   );
 };
